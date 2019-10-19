ec1c4e63336b8477bbb53732eb9f3b8cf927e2c4
davidedantonio <davide.dantonio@webeetle.com>
2019-10-19 16:28:15 +0200

    enh

--- nexec.js
+++ nexec.js
@@ -15,7 +15,7 @@ console.log(chalk.yellow(figlet.textSync('nexec', { horizontalLayout: 'full' }))
 program
   .name('nexec')
   .command('register [options]', 'Register your favourite command').alias('r')
-  .command('execute [options]', 'Exec the command name given in input').alias('e')
+  .command('execute [options]', 'Exec the command name given in input', { isDefault: true }).alias('e')
   .command('delete', 'Delete the given command name').alias('d')
   .command('list', 'Show all registered command').alias('l')
   .parse(process.argv)
